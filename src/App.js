78b98f4988204ed40e0bb0aee0e7307fc6b5ac25
ISMAIL CAN <caniscanmail@gmail.com>
2025-07-07 12:38:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ import AddRecord from "./components/AddRecord";
 
 function App() {
   return (
-  <HashRouter>
+
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
@@ -39,7 +39,7 @@ function App() {
         <Route path="records/add" element={<AddRecord />} />
       </Route>
     </Routes>
-  </HashRouter>
+  
   );
 }
 
